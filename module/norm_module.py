d18b03abfc8882ca9b8bd64a8147d4cb0b9dd039
Nemo <306250675@qq.com>
2026-03-28 22:20:23 +0800

    normalization

--- module/norm_module.py
+++ module/norm_module.py
@@ -96,3 +96,15 @@ class LayerNorm(nn.Module):
             output = self.gamma * output + self.beta
         return output
 
+class RMSNorm(nn.Module):
+    def __init__(self, dim: int, eps: float = 1e-6):
+        super().__init__()
+        self.dim = dim
+        self.eps = eps
+        self.weight = nn.Parameter(torch.ones(dim))
+
+    def _norm(self, x):
+        return x * torch.rsqrt(x.pow(2).mean(-1, keepdim=True) + self.eps)
+
+    def forward(self, x):
+        return (self.weight * self._norm(x.float())).type_as(x)
